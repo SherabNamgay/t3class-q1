0cdf2e6b541e5fa08eb924646e49faec7ff59e57
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-02-14 11:47:05 +0600

    new

--- stringMethods.js
+++ stringMethods.js
@@ -1,2 +1,29 @@
-var results="tom is sitting at his computer".split("")
-console.log(results)
+// var result = "Tom is at his computer.".split("")
+// const restrictedWords = ["banana", "orange", "apple", "peach", "grape"]
+
+// function checkUsername(username) {
+// 	var isUserNameOk = true
+
+// 	for (var i = 0; i < restrictedWords.length; i += 1) {
+// 		var includesRestrictedWord = username.includes(restrictedWords[i])
+// 		if (includesRestrictedWord) {
+// 			isUserNameOk = false
+// 		}
+// 	}
+
+// 	return isUserNameOk
+// }
+
+// console.log(checkUsername("mike45")) // true
+// console.log(checkUsername("apple45")) // false
+
+var userName = "john hames fones".split(" ")
+
+for (var i=0; i<userName.length; i++){
+    var singleNames=userName[i].split("")
+    // singleNames[0]=singleNames[0].toUpperCase()
+retrun singleNames
+}
+console.log(singleName)
+console.log(userName)
+
