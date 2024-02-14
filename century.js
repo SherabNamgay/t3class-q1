8aecf314ab29b94bc1f2b7e960ed71735694415e
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-02-14 15:29:51 +0600

    new centuary file

---
+++ century.js
@@ -0,0 +1,22 @@
+
+
+var rl=require("readline-sync");
+var year= Number(rl.question("which year you want to know the century of?\n"))
+function whichCenturyIs(year){
+    var century=Math.ceil(year/100)
+    var suffix = "th"
+	if (century % 100 >= 10 && century % 100 <= 20) {
+		suffix = "th"
+	} else if (century % 10 === 1) {
+		suffix = "st"
+	} else if (century % 10 === 2) {
+		suffix = "nd"
+	} else if (century % 10 === 3) {
+		suffix = "rd"
+	}
+
+	return century + suffix + " century"
+}
+
+var century = 
+
