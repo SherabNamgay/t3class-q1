08f0f92cac23fcfd99b7e699a54555e3887dd16c
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-02-14 11:59:20 +0600

    chaninging the first letter of the names using array and for loop

--- stringMethods.js
+++ stringMethods.js
@@ -17,13 +17,28 @@
 // console.log(checkUsername("mike45")) // true
 // console.log(checkUsername("apple45")) // false
 
-var userName = "john hames fones".split(" ")
+var result = "john hames fones"
 
-for (var i=0; i<userName.length; i++){
-    var singleNames=userName[i].split("")
-    // singleNames[0]=singleNames[0].toUpperCase()
-retrun singleNames
+// for (var i=0; i<userName.length; i++){
+//     var singleNames=userName[i].split("")
+//     // singleNames[0]=singleNames[0].toUpperCase()
+// retrun singleNames
+// }
+// console.log(singleName)
+// console.log(userName)
+
+
+//  SOLUTION !!!!!
+var splitIntoWords = result.split(" ")
+
+for (var i = 0; i < splitIntoWords.length; i ++) {
+	var currentWord = splitIntoWords[i]
+	var lettersOfCurrentWord = currentWord.split("")
+    lettersOfCurrentWord[0] = lettersOfCurrentWord[0].toLocaleUpperCase() // diff
+	// var capitalizedLetter = lettersOfCurrentWord[0].toLocaleUpperCase()
+	// lettersOfCurrentWord[0] = capitalizedLetter
+	var capitalizedWord = lettersOfCurrentWord.join("")
+	splitIntoWords[i] = capitalizedWord
 }
-console.log(singleName)
-console.log(userName)
-
+const capitalizedName = splitIntoWords.join(" ")
+console.log(capitalizedName)
