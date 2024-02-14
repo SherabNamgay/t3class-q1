1c2e6945a81eb30190841a47bba399c23bf128ca
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-02-14 09:55:49 +0600

    new files

--- addBinary.js
+++ addBinary.js
@@ -15,7 +15,21 @@ function addBinary(binA, binB) {
 
 const a = [1, 0, 1, 1, 0, 1]
 const b = [0, 1, 1, 0, 0, 0]
-addBinary(a,b)
+// addBinary(a,b)
 
 // addBinary(a, b) === [1, 0, 0, 0, 1, 0, 1]
+function subtractBinary(binA,binB){
+    var ans=[]
+    for (var i=binA.length-1; i>=0; i--){
+        ans[i]=binA[i]-binB[i]
+    }
+    console.log(ans)
+    for (var i=ans.length-1; i>=0; i--){
+        if (ans[i]<0){
+            ans[i]=1
+            ans[i-1]-=1
+        }
+    }
+}
 
+subtractBinary(a,b)
---
+++ try-catch.js
@@ -0,0 +1,11 @@
+try{
+    // try{
+    i+1
+    console.log("will print here")
+    //  } catch (e){
+    // console.log("Here?")
+    //  } 
+} catch (e){
+    console.log(e)
+}
+console.log("I want the program to continue")
