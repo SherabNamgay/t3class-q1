62c1e7d6f354f77cd34cf578c84fc63409b980d4
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-02-14 12:59:37 +0600

    subtraction of binary

--- addBinary.js
+++ addBinary.js
@@ -12,24 +12,28 @@ function addBinary(binA, binB) {
     }
     console.log(ans)
 }
+addBinary(a,b)
+
+addBinary(a, b) === [1, 0, 0, 0, 1, 0, 1]
 
 const a = [1, 0, 1, 1, 0, 1]
-const b = [0, 1, 1, 0, 0, 0]
-// addBinary(a,b)
-
-// addBinary(a, b) === [1, 0, 0, 0, 1, 0, 1]
+const b = [0, 1, 1, 0, 1, 0]
+//  010101
 function subtractBinary(binA,binB){
-    var ans=[]
+    var diff=[]
     for (var i=binA.length-1; i>=0; i--){
-        ans[i]=binA[i]-binB[i]
+        diff[i]=binA[i]-binB[i]
     }
-    console.log(ans)
-    for (var i=ans.length-1; i>=0; i--){
-        if (ans[i]<0){
-            ans[i]=1
-            ans[i-1]-=1
+    for (var i=diff.length-1; i>=0; i--){
+        if (diff[i]<0){
+            diff[i]=diff[i]+2 
+            diff[i-1]-=1
         }
     }
+    console.log(diff);
 }
+subtractBinary(a,b);
 
-subtractBinary(a,b)
+function numToBin(num){
+    
+}
