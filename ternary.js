371217263049625532ec7d2a799578fe67c98db6
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-02-15 09:33:57 +0600

    adds new file ternary

---
+++ ternary.js
@@ -0,0 +1,8 @@
+if (true) console.log("hello")
+else console,log("goodbye")
+
+console.log(true? "hello": "goodbye")  // can be used without {} if the statement contains only one condition
+
+cons x=1 
+cons plural = x<1 ? "s" : ""      // ternary ... same as if (?) and else(:)
+console.log("the sting has "+ "vowel" +plural)
